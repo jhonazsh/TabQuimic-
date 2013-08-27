c36016cfdb0d0f3ee01e1721da8488b0de96b294
jhon medina <jhofe_s17@hotmail.com>
2013-08-27 17:37:44 -0500

    primer

---
+++ js/show-big-elementos.js
@@ -0,0 +1,152 @@
+$(document).ready(function(){
+
+	$("#h").mouseenter(function(){
+		document.getElementById('big-h-num').innerHTML=elementos[0].numero;
+		document.getElementById('big-h-simb').innerHTML=elementos[0].simbolo;
+		document.getElementById('big-h-nomb').innerHTML=elementos[0].nombre;
+		$(".gran-h").show(50);
+	});
+	$("#h").mouseleave(function(){
+		$(".gran-h").hide(50);
+	});
+
+	$("#he").mouseenter(function(){
+		document.getElementById('big-he-num').innerHTML=elementos[1].numero;
+		document.getElementById('big-he-simb').innerHTML=elementos[1].simbolo;
+		document.getElementById('big-he-nomb').innerHTML=elementos[1].nombre;
+		$(".gran-he").show(50);
+	});
+	$("#he").mouseleave(function(){
+		$(".gran-he").hide(50);
+	});
+
+	$("#li").mouseenter(function(){
+		document.getElementById('big-li-num').innerHTML=elementos[2].numero;
+		document.getElementById('big-li-simb').innerHTML=elementos[2].simbolo;
+		document.getElementById('big-li-nomb').innerHTML=elementos[2].nombre;
+		$(".gran-li").show(50);
+	});
+	$("#li").mouseleave(function(){
+		$(".gran-li").hide(50);
+	});
+
+	$("#be").mouseenter(function(){
+		document.getElementById('big-be-num').innerHTML=elementos[3].numero;
+		document.getElementById('big-be-simb').innerHTML=elementos[3].simbolo;
+		document.getElementById('big-be-nomb').innerHTML=elementos[3].nombre;
+		$(".gran-be").show(50);
+	});
+	$("#be").mouseleave(function(){
+		$(".gran-be").hide(50);
+	});
+
+	$("#b").mouseenter(function(){
+		document.getElementById('big-b-num').innerHTML=elementos[4].numero;
+		document.getElementById('big-b-simb').innerHTML=elementos[4].simbolo;
+		document.getElementById('big-b-nomb').innerHTML=elementos[4].nombre;
+		$(".gran-b").show(50);
+	});
+	$("#b").mouseleave(function(){
+		$(".gran-b").hide(50);
+	});
+
+	$("#c").mouseenter(function(){
+		document.getElementById('big-c-num').innerHTML=elementos[5].numero;
+		document.getElementById('big-c-simb').innerHTML=elementos[5].simbolo;
+		document.getElementById('big-c-nomb').innerHTML=elementos[5].nombre;
+		$(".gran-c").show(50);
+	});
+	$("#c").mouseleave(function(){
+		$(".gran-c").hide(50);
+	});
+
+	$("#n").mouseenter(function(){
+		document.getElementById('big-n-num').innerHTML=elementos[6].numero;
+		document.getElementById('big-n-simb').innerHTML=elementos[6].simbolo;
+		document.getElementById('big-n-nomb').innerHTML=elementos[6].nombre;
+		$(".gran-n").show(50);
+	});
+	$("#n").mouseleave(function(){
+		$(".gran-n").hide(50);
+	});
+
+	$("#o").mouseenter(function(){
+		document.getElementById('big-o-num').innerHTML=elementos[7].numero;
+		document.getElementById('big-o-simb').innerHTML=elementos[7].simbolo;
+		document.getElementById('big-o-nomb').innerHTML=elementos[7].nombre;
+		$(".gran-o").show(50);
+	});
+	$("#o").mouseleave(function(){
+		$(".gran-o").hide(50);
+	});
+
+	$("#f").mouseenter(function(){
+		document.getElementById('big-f-num').innerHTML=elementos[8].numero;
+		document.getElementById('big-f-simb').innerHTML=elementos[8].simbolo;
+		document.getElementById('big-f-nomb').innerHTML=elementos[8].nombre;
+		$(".gran-f").show(50);
+	});
+	$("#f").mouseleave(function(){
+		$(".gran-f").hide(50);
+	});
+
+	$("#ne").mouseenter(function(){
+		document.getElementById('big-ne-num').innerHTML=elementos[9].numero;
+		document.getElementById('big-ne-simb').innerHTML=elementos[9].simbolo;
+		document.getElementById('big-ne-nomb').innerHTML=elementos[9].nombre;
+		$(".gran-ne").show(50);
+	});
+	$("#ne").mouseleave(function(){
+		$(".gran-ne").hide(50);
+	});
+
+	$("#na").mouseenter(function(){
+		document.getElementById('big-na-num').innerHTML=elementos[10].numero;
+		document.getElementById('big-na-simb').innerHTML=elementos[10].simbolo;
+		document.getElementById('big-na-nomb').innerHTML=elementos[10].nombre;
+		$(".gran-na").show(50);
+	});
+	$("#na").mouseleave(function(){
+		$(".gran-na").hide(50);
+	});
+
+	$("#mg").mouseenter(function(){
+		document.getElementById('big-mg-num').innerHTML=elementos[11].numero;
+		document.getElementById('big-mg-simb').innerHTML=elementos[11].simbolo;
+		document.getElementById('big-mg-nomb').innerHTML=elementos[11].nombre;
+		$(".gran-mg").show(50);
+	});
+	$("#mg").mouseleave(function(){
+		$(".gran-mg").hide(50);
+	});
+
+	$("#al").mouseenter(function(){
+		document.getElementById('big-al-num').innerHTML=elementos[12].numero;
+		document.getElementById('big-al-simb').innerHTML=elementos[12].simbolo;
+		document.getElementById('big-al-nomb').innerHTML=elementos[12].nombre;
+		$(".gran-al").show(50);
+	});
+	$("#al").mouseleave(function(){
+		$(".gran-al").hide(50);
+	});
+
+	$("#si").mouseenter(function(){
+		document.getElementById('big-si-num').innerHTML=elementos[13].numero;
+		document.getElementById('big-si-simb').innerHTML=elementos[13].simbolo;
+		document.getElementById('big-si-nomb').innerHTML=elementos[13].nombre;
+		$(".gran-si").show(50);
+	});
+	$("#si").mouseleave(function(){
+		$(".gran-si").hide(50);
+	});
+
+	$("#p").mouseenter(function(){
+		document.getElementById('big-p-num').innerHTML=elementos[14].numero;
+		document.getElementById('big-p-simb').innerHTML=elementos[14].simbolo;
+		document.getElementById('big-p-nomb').innerHTML=elementos[14].nombre;
+		$(".gran-p").show(50);
+	});
+	$("#p").mouseleave(function(){
+		$(".gran-p").hide(50);
+	});
+});
