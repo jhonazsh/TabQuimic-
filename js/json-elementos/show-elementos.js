3bb658d59dbfcb087588c50ee60a47c4c7175725
jhon medina <jhofe_s17@hotmail.com>
2013-08-30 12:06:04 -0500

    cambios de prueba

--- js/json-elementos/show-elementos.js
+++ js/json-elementos/show-elementos.js
@@ -13,8 +13,7 @@ while (i < 118) {
 	$('.cel')[a].children[2].innerHTML = elementos[i].nombre;
 
 	$( ".cel" ).each(function( a ) {
-		$(this).data('peso', elementos[a].peso)
-		console.log( a + ": " + $(this).text() );
+		$(this).data('peso', elementos[a].peso);
 	});
 	
 
@@ -32,11 +31,33 @@ function mostrar() {
 	$('.big-simbolo').html($this[1].innerHTML);
 	$('.big-nombre').html($this[2].innerHTML);
 
-	var a
 	$('.big-others').html($(this).data('peso'));
-
 }
 
+/*empezamos con la coloracion*/
+
+$("#metales").on("click", colorearMetal);
+$("#nometales").on("click", colorearNoMetal);
+$("#metaloides").on("click", colorearMetaloide);
+
+function colorearMetal(){
+	$(".metal").css({"background-color":"rgb(123, 170, 63)"});
+	$(".nometal").css({"background-color":"rgb(204, 204, 154)"});
+	$(".metaloide").css({"background-color":"rgb(204, 204, 154)"});
+}
+
+function colorearNoMetal(){
+	$(".nometal").css({"background-color":"rgb(196, 196, 83)"});
+	$(".metal").css({"background-color":"rgb(204, 204, 154)"});
+	$(".metaloide").css({"background-color":"rgb(204, 204, 154)"});
+}
+
+
+function colorearMetaloide(){
+	$(".metaloide").css({"background-color":"rgb(108, 108, 221)"});
+	$(".metal").css({"background-color":"rgb(204, 204, 154)"});
+	$(".nometal").css({"background-color":"rgb(204, 204, 154)"});
+}
 
 
 
