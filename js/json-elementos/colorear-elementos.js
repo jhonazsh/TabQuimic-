dc73b7fff93a606e0ffc83e3da0fd9bb60fa8299
jhon medina <jhofe_s17@hotmail.com>
2013-09-30 12:07:57 -0500

    cambios actuales del 30/09/2013 a las 12:07 p.m.

--- js/json-elementos/colorear-elementos.js
+++ js/json-elementos/colorear-elementos.js
@@ -7,7 +7,7 @@ function colorearMetal(){
 
 	$('.cel').each(function() {
 		if ($(this).data('grupo') == 'metales') {
-			$(this).css({'background':'rgb(76, 211, 64)'});
+			$(this).css({'background':'rgb(167, 163, 255)'});
 		}
 	});
 }
@@ -31,24 +31,104 @@ function colorearMetaloide(){
 }
 
 
-$('#metales').on("click",mostrarMenuMetales);
-$('#nometales').on("click",mostrarMenuNoMetales);
-$('#metaloides').on("click",mostrarMenuMetaloides);
+$('#metales').on("click",{id:1},mostrarMenu);
+$('#nometales').on("click",{id:2},mostrarMenu);
+$('#metaloides').on("click",{id:3},mostrarMenu);
 
-function mostrarMenuMetales(){
-	$('.glyphicon-chevron-down').eq(0).toggleClass('glyphicon-chevron-up');
-	$('.submenuno').toggle();
-}
+function mostrarMenu(valor,valoruno) {
+	if(valor.data.id==1){
+		$('.glyphicon-chevron-down').eq(0).toggleClass('glyphicon-chevron-up');
+		$('.submenuno').toggle();
+		console.log('hola');
+	}
 
-function mostrarMenuNoMetales(){
-	$('#nometales .glyphicon-chevron-down').removeClass('nometales glyphicon-chevron-down').addClass('nometales glyphicon-chevron-up');
-	$('.submendos').toggle();
-}
+	else if(valor.data.id==2){
+		$('.glyphicon-chevron-down').eq(1).toggleClass('glyphicon-chevron-up');
+		$('.submendos').toggle();
+	}
 
-function mostrarMenuMetaloides(){
-	$('#metaloides .glyphicon-chevron-down').removeClass('metaloides glyphicon-chevron-down').addClass('metaloides glyphicon-chevron-up');
-	$('.submentres').toggle();
+	else if(valor.data.id==3){
+		$('.glyphicon-chevron-down').eq(2).toggleClass('glyphicon-chevron-up');
+		$('.submentres').toggle();
+	}
 }
 
 ////////////////////////////////////
 
+$('.fil').eq(0).on('mouseover',{id:1},colorGrupo);
+$('.fil').eq(1).on('mouseover',{id:2},colorGrupo);
+$('.fil').eq(2).on('mouseover',{id:3},colorGrupo);
+$('.fil').eq(3).on('mouseover',{id:4},colorGrupo);
+$('.fil').eq(4).on('mouseover',{id:5},colorGrupo);
+$('.fil').eq(5).on('mouseover',{id:6},colorGrupo);
+$('.fil').eq(6).on('mouseover',{id:7},colorGrupo);
+
+$('.fil').eq(0).on('mouseleave',{id:1},descolorGrupo);
+$('.fil').eq(1).on('mouseleave',{id:2},descolorGrupo);
+$('.fil').eq(2).on('mouseleave',{id:3},descolorGrupo);
+$('.fil').eq(3).on('mouseleave',{id:4},descolorGrupo);
+$('.fil').eq(4).on('mouseleave',{id:5},descolorGrupo);
+$('.fil').eq(5).on('mouseleave',{id:6},descolorGrupo);
+$('.fil').eq(6).on('mouseleave',{id:7},descolorGrupo);
+
+function colorGrupo(valorsito) {
+	if (valorsito.data.id==1) {
+		$('.menors').eq(0).css({'background-color':'black'});
+	}
+
+	else if (valorsito.data.id==2) {
+		$('.menors').eq(1).css({'background-color':'black'});
+	}
+
+	else if (valorsito.data.id==3) {
+		$('.menors').eq(2).css({'background-color':'black'});
+	}
+
+	else if (valorsito.data.id==4) {
+		$('.menors').eq(3).css({'background-color':'black'});
+	}
+
+	else if (valorsito.data.id==5) {
+		$('.menors').eq(4).css({'background-color':'black'});
+	}
+
+	else if (valorsito.data.id==6) {
+		$('.menors').eq(5).css({'background-color':'black'});
+	}
+
+	else if (valorsito.data.id==7) {
+		$('.menors').eq(6).css({'background-color':'black'});
+	}
+	
+}
+
+function descolorGrupo(valorsito) {
+
+	if (valorsito.data.id==1) {
+		$('.menors').eq(0).css({'background-color':'rgba(25, 61, 34, 0.43)'});
+	}
+
+	else if (valorsito.data.id==2) {
+		$('.menors').eq(1).css({'background-color':'rgba(25, 61, 34, 0.43)'});
+	}
+
+	else if (valorsito.data.id==3) {
+		$('.menors').eq(2).css({'background-color':'rgba(25, 61, 34, 0.43)'});
+	}
+
+	else if (valorsito.data.id==4) {
+		$('.menors').eq(3).css({'background-color':'rgba(25, 61, 34, 0.43)'});
+	}
+
+	else if (valorsito.data.id==5) {
+		$('.menors').eq(4).css({'background-color':'rgba(25, 61, 34, 0.43)'});
+	}
+
+	else if (valorsito.data.id==6) {
+		$('.menors').eq(5).css({'background-color':'rgba(25, 61, 34, 0.43)'});
+	}
+
+	else if (valorsito.data.id==7) {
+		$('.menors').eq(6).css({'background-color':'rgba(25, 61, 34, 0.43)'});
+	}
+}
